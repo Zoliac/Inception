e757b1faff242e87e534954845341022beabd2b3
leny <lenypatin@gmail.com>
2026-08-06 11:49:10 +0200

    Fixed mariadb's entrypoint

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,19 +1,29 @@
+#!/bin/sh
 set -e
+# si pas de volumes ni de data, on en met dedans
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MariaDB data directory..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
-/usr/bin/mariadbd-safe --datadir=/var/lib/mysql &
+    # demarage tempo de maria pour mettre la data sql dedans
+    mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking=0 &
+    pid="$!"
 
-echo "Waiting for MariaDB to be ready..."
-until mariadb-admin ping --silent; do
-	sleep 2
-done
-echo "MariaDB is ready"
+    # On attend que ce process temporaire réponde
+    until mariadb-admin ping --silent 2>/dev/null; do
+        sleep 1
+    done
 
-export MARIADB_PASSWORD=$(cat /run/secrets/db_password)
-export MARIADB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+    export MARIADB_PASSWORD=$(cat /run/secrets/db_password)
+    export MARIADB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-if [ -f "/tools/initdb.sql" ]; then
-	envsubst < /tools/initdb.sql | mariadb
-	
+    if [ -f "/tools/initdb.sql" ]; then
+        envsubst < /tools/initdb.sql | mariadb -u root
+    fi
+
+    # arret de l'instance tempo
+    mariadb-admin -u root shutdown
+    wait "$pid"   # wait ok parce que on attend la fin de l'instance et pas la fin du programme pid1
+    echo "Initialization done."
 fi
-echo "MariaDB is ready"
-wait
+exec mariadbd --user=mysql --datadir=/var/lib/mysql
